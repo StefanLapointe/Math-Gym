996c7c68cf2f57a9d085f7ac90e3383d53010156
Stefan Lapointe <stefan.lapointe@mail.mcgill.ca>
2025-05-07 12:50:02 -0400

    Initialize Spring Boot subproject

---
+++ backend/src/main/java/com/stefanlapointe/mathgym/MathGymApplication.java
@@ -0,0 +1,13 @@
+package com.stefanlapointe.mathgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathGymApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stefanlapointe/mathgym/MathGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefanlapointe.mathgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

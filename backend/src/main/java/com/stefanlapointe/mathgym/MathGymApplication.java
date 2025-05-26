39081935d36214be87b22444eaaa5da7f9496985
Stefan Lapointe <stefan.lapointe@mail.mcgill.ca>
2025-05-26 13:10:47 -0400

    Improve style

--- backend/src/main/java/com/stefanlapointe/mathgym/MathGymApplication.java
+++ backend/src/main/java/com/stefanlapointe/mathgym/MathGymApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MathGymApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MathGymApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MathGymApplication.class, args);
+    }
 
 }

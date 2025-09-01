bf984f8518a877a28233d199500c9bd239e2bad9
Stefan Lapointe <stefan.lapointe@mail.mcgill.ca>
2025-08-31 23:04:49 -0400

    Add response submission and evaluation

--- frontend/src/app/components/problem/problem.ts
+++ frontend/src/app/components/problem/problem.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,8 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Problem implements OnInit {
   statement = "Loading...";
+  private seed = 0;
   private route = inject(ActivatedRoute);
   private problemType = "";
+  correction = "";
+  corrected = signal(false);
+  buttonText = computed(() => this.corrected() ? "Next" : "Submit!");
+  completed = 0;
+  correct = 0;
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.problemType = params["problemType"];
@@ -18,6 +24,10 @@ export class Problem implements OnInit {
     this.next();
   }
   next() {
+    const answerBox = document.getElementById("answer-box") as HTMLInputElement;
+    answerBox.value = "";
+    this.correction = "";
+    this.corrected.set(false);
     fetch("http://localhost:8080/api/generate", {
       method: "POST",
       headers: {
@@ -28,6 +38,29 @@ export class Problem implements OnInit {
       .then(response => response.json())
       .then(json => {
         this.statement = json.statement;
+        this.seed = json.seed;
       });
+  }
+  evaluate() {
+    const answerBox = document.getElementById("answer-box") as HTMLInputElement;
+    const response = {
+      problemType: this.problemType,
+      seed: this.seed,
+      response: answerBox.value
+    }
+    fetch("http://localhost:8080/api/evaluate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(response)
+    })
+      .then(response => response.json())
+      .then(problemCorrection => {
+        this.correction = problemCorrection.correction;
+        this.corrected.set(true);
+        this.completed++;
+        if (problemCorrection.correct) this.correct++;
+      })
   }
 }

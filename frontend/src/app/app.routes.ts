528f16b7bed4f57d0b358be51d0c9382242ae1c6
Stefan Lapointe <stefan.lapointe@mail.mcgill.ca>
2025-09-05 03:56:01 -0400

    Move pages/home to home/home-page to follow feature based project structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { HomePage } from './home/home-page/home-page';
 import { TrainingPage } from './math-problems/training-page/training-page';
 
 export const routes: Routes = [
-    {path: "", component: Home},
+    {path: "", component: HomePage},
     {path: "training/:problemType", component: TrainingPage}
 ];
--- frontend/src/app/home/home-page/home-page.ts
+++ frontend/src/app/home/home-page/home-page.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ProblemTypeList } from "../../math-problems/problem-type-list/problem-type-list";
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home-page',
   imports: [ProblemTypeList],
-  templateUrl: './home.html',
-  styleUrl: './home.css'
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
 })
-export class Home {
+export class HomePage {
 
 }
